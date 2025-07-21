ee32a8cc22390e014961a9f5677fb3c3e4239598
hari123-hh <harichandane555@gmail.com>
2025-07-21 22:34:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (e) {
+    alert("✅ Thank you! Your message has been sent successfully.");
+  });
+});
